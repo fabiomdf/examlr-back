97d2012d7864f19f51b6bd5986e3de4f902bd68c
fabiomdf <fabiomdf@gmail.com>
2017-05-30 20:52:30 -0300

    0.0.1

---
+++ common/models/question.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = function (Question) {
+/*
+    http://localhost:3000/api/Groups?filter[include][categories][questions]&filter[where][id]=5920c91012623f0b0a16e2c2
+    
+    Question.status = function (cb) {
+        server.models.Group.find({
+            fields: {
+                id: '5920c91012623f0b0a16e2c2'
+            }
+        }, cb);
+    }
+
+    Question.remoteMethod(
+        'status', {
+            http: {
+                path: '/status',
+                verb: 'get'
+            },
+            returns: [
+                { arg: 'group', type: 'array' }
+            ],
+        }
+    )
+*/
+};
