392db9c1e5118f0afdae85b51e123779883e6d42
fabiomdf <fabiomdf@gmail.com>
2017-07-29 17:42:17 -0300

    user related to groups and questions

--- common/models/group.js
+++ common/models/group.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = function(Group) {
-
+  /** Este remotehook inclui o id do usuario logado na tabela de grupos quando um grupo é criado */
+  Group.beforeRemote('create', function(context, user, next) {
+    // context.args.data.createdAt = Date.now();
+    context.args.data.userId = context.req.accessToken.userId;
+    next();
+  });
 };
--- common/models/question.js
+++ common/models/question.js
@@ -1,27 +1,31 @@
 'use strict';
 
-module.exports = function (Question) {
-/*
-    http://localhost:3000/api/Groups?filter[include][categories][questions]&filter[where][id]=5920c91012623f0b0a16e2c2
-    
-    Question.status = function (cb) {
-        server.models.Group.find({
-            fields: {
-                id: '5920c91012623f0b0a16e2c2'
-            }
-        }, cb);
-    }
+module.exports = function(Question) {
+  /** Este remotehook inclui o id do usuario logado na tabela de questions quando uma pergunta é criada */
+  Question.beforeRemote('create', function(context, user, next) {
+    // context.args.data.createdAt = Date.now();
+    context.args.data.userId = context.req.accessToken.userId;
+    next();
+  });
 
-    Question.remoteMethod(
-        'status', {
-            http: {
-                path: '/status',
-                verb: 'get'
-            },
-            returns: [
-                { arg: 'group', type: 'array' }
-            ],
-        }
-    )
-*/
+    // http://localhost:3000/api/Groups?filter[include][categories][questions]&filter[where][id]=5920c91012623f0b0a16e2c2
+    // Question.status = function (cb) {
+    //     server.models.Group.find({
+    //         fields: {
+    //             id: '5920c91012623f0b0a16e2c2'
+    //         }
+    //     }, cb);
+    // }
+    // Question.remoteMethod(
+    //     'status', {
+    //         http: {
+    //             path: '/status',
+    //             verb: 'get'
+    //         },
+    //         returns: [
+    //             { arg: 'group', type: 'array' }
+    //         ],
+    //     }
+    // )
+
 };
