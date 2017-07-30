47ff8ddbe0eb50e96ad2cfd29630e75fe5a80e0b
fabiomdf <fabiomdf@gmail.com>
2017-07-30 12:38:32 -0300

    user related to category and answer

--- common/models/answer.js
+++ common/models/answer.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = function(Answer) {
-
+  Answer.beforeRemote('create', function(context, user, next) {
+    context.args.data.userId = context.req.accessToken.userId;
+    next();
+  });
 };
--- common/models/category.js
+++ common/models/category.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = function(Category) {
-
+  Category.beforeRemote('create', function(context, user, next) {
+    context.args.data.userId = context.req.accessToken.userId;
+    next();
+  });
 };
